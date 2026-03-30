84dcc90a8209dd86b46c63ac0a787a6ba578de94
nolanribet <nolan.ribet05@gmail.com>
2026-03-30 11:22:20 +0200

    Ajout de la favicon sur le site

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>4Learning - Plateforme communautaire</title>
-    <link rel="icon" href="../assets/img/Logo 4Learn .png" />
+    <link rel="icon" href="./assets/img/favicon.png" />
 </head>
 <body>  
     <p>Page d'accueil</p> 
