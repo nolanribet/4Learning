2f91fcc95b96919cf747f412e64f741caf79692c
nolanribet <nolan.ribet05@gmail.com>
2026-03-30 11:06:11 +0200

    Ajout de l'index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4Learning - Plateforme communautaire</title>
+    <link rel="icon" href="../assets/img/Logo 4Learn .png" />
+</head>
+<body>  
+    <p>Page d'accueil</p> 
+</body>
+</html>
