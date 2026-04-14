e8f0eed569c78cfa564df75225de3ad0b3241871
Yanis Novel <yanisnovel@eduroam-n2ng-144186.local.univ-savoie.fr>
2026-04-14 17:39:41 +0200

    fix

--- php/mvc_tags/crud_tags.php
+++ php/mvc_tags/crud_tags.php
@@ -1,19 +1,19 @@
 <?php
 
 function createTags($conn, $name, $color){
-    $sql = "INSERT INTO `tags_matiere`(`name`,`color`) VALUES('$name','$color')";
+    $sql = "INSERT INTO `tags_matiere` (`name`, `color`) VALUES ('$name', '$color')";
     $req = mysqli_query($conn, $sql);
     return $req;
 }
 
 function updateTags($conn, $name, $color, $id){
-    $sql = "UPDATE `tags_matiere` SET `name`='$name', `couleur`='$color' WHERE `id`='$id'";
+    $sql = "UPDATE `tags_matiere` SET `name`='$name', `color`='$color' WHERE `id`='$id'";
     $req = mysqli_query($conn, $sql);
     return $req;
 }
 
 function readTags($conn, $id){
-    $sql = "SELECT * from `tags_matiere` WHERE `id`='$id'";
+    $sql = "SELECT * FROM `tags_matiere` WHERE `id`='$id'";
     $req = mysqli_query($conn, $sql);
     return $req;
 }
@@ -25,7 +25,7 @@ function deleteTags($conn, $id){
 }
 
 function listTags($conn){
-    $sql = "SELECT * FROM `tags_matiere`";
+    $sql = "SELECT * FROM `tags_matiere` ORDER BY name ASC"; // Petit bonus : trié par nom 🗂️
     $req = mysqli_query($conn, $sql);
     return $req;
 }
